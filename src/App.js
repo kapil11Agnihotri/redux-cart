905bc6a974b32b104abb5a4996b9777c2c978933
kapil11Agnihotri <kapil90444@gmail.com>
2022-11-14 23:47:14 +0530

    notification added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,78 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import axios from "axios";
+import { uiActions } from "./components/store/ui-slice";
+import Notification from "./components/UI/Notification";
 
+let isInitial=true;
 
 function App() {
- const showCart = useSelector((state)=>state.ui.cartIsVisible)
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.showNotification);
+  
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Pending",
+          title: "Sending",
+          message: "Sending Cart Data",
+        })
+      );
+      const responce = await axios.put(
+        "https://my-http-request-908a9-default-rtdb.firebaseio.com/cart.json",
+        cart
+      );
+      if (!responce.ok) {
+        throw new Error("Sending cart is failed");
+      }else{
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success!",
+            message: "Sent cart data successfully",
+          })
+        );
+      }
+     
+    };
+    if(isInitial){
+      isInitial=false
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products/>
-    </Layout>
+    <Fragment>
+      
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
